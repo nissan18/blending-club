c538f91a5c5284c4d75840771f0e77cfd0291a59
Michael Rosenzweig <mrosenzweig3@bloomberg.net>
2018-07-14 22:47:58 -0400

    push

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 3. validate, retrain, test
 
 Link to Lending Club API - https://www.lendingclub.com/developers/listed-loans
+
+
+### QUESTION: How do deal with one-hot encoding columns?
+Should I hard-code all possible values, build a model and then validate any input into model or build them on a fly? Since I need to think about it, let's ignore one-hot encoding columns for now.
+
--- code/lendingclub_pipeline.py
+++ code/lendingclub_pipeline.py
@@ -1,4 +1,6 @@
 import pandas_helper
+import scikit_helper
+
 import lendingclub_columns
 import lendingclub_helper
 
@@ -18,13 +20,31 @@ class LendingClub_Pipeline:
 
         df_train = pandas_helper.readData(trainset_filename, lendingclub_columns.get_dtypes_by_name())
 
-        self.buildFeatures(df_train)
+        self.buildFeaturesAndLabels(df_train)
+
+        feature_columns = lendingclub_helper.getFeatureColumns(df_train)
+        label_columns = lendingclub_helper.getLabelColumns(df_train)
+
+        print("features: {}".format(feature_columns))
+        print("labels: {}".format(label_columns))
 
 
-    def buildFeatures(self, df):
+        X = df_train[feature_columns].values
+        y = df_train[label_columns[0]].values
+        # print(df_train[['f_annual_inc', 'f_sub_grade_A1', 'f_sub_grade_A2', 'f_sub_grade_A3', 'f_sub_grade_A4', 'f_sub_grade_A5', 'f_sub_grade_B1', 'f_sub_grade_B2', 'f_sub_grade_B3', 'f_sub_grade_B4', 'f_sub_grade_B5', 'f_sub_grade_C1', 'f_sub_grade_C2', 'f_sub_grade_C3', 'f_sub_grade_C4', 'f_sub_grade_C5', 'f_sub_grade_D1', 'f_sub_grade_D2', 'f_sub_grade_D3', 'f_sub_grade_D4', 'f_sub_grade_D5', 'f_sub_grade_E1', 'f_sub_grade_E2', 'f_sub_grade_E3', 'f_sub_grade_E4', 'f_sub_grade_E5', 'f_sub_grade_F1', 'f_sub_grade_F2', 'f_sub_grade_F4', 'f_sub_grade_F5', 'f_sub_grade_G1', 'f_sub_grade_G2', 'f_sub_grade_F3', 'f_sub_grade_G4', 'f_sub_grade_G5', 'f_sub_grade_NULL']])
+        # print(df_train[feature_columns])
+        # print(df_train[[label_columns]])
+        # print(df_train[feature_columns + label_columns])
 
+        model = scikit_helper.trainModel(X, y)
+        print(model)
+
+
+
+    def buildFeaturesAndLabels(self, df):
         feature_columns = lendingclub_columns.get_columns_by_names(["annual_inc", "sub_grade"])
+        # feature_columns = lendingclub_columns.get_columns_by_names(["annual_inc"])
         lendingclub_helper.build_features_by_columns(df, feature_columns)
+        lendingclub_helper.buildCategorialLabel(df, "loan_status", lendingclub_helper.parse_loan_status)
 
 
-        pass
