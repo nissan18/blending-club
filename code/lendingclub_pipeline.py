34983b8383d66487057928cb4cd43aaa3b05cea3
Michael Rosenzweig <mrosenzweig3@bloomberg.net>
2018-07-18 06:45:08 -0400

    build simple pipeline and check accuracy

--- code/lendingclub_pipeline.py
+++ code/lendingclub_pipeline.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 import pandas_helper
 import scikit_helper
 
@@ -8,43 +10,67 @@ import lendingclub_helper
 class LendingClub_Pipeline:
 
     def __init__(self):
-        pass
+        self.featurize_columns = ["annual_inc", "sub_grade"]
+        self.labelize_columns = ["loan_status"]
 
 
     def run(self, trainset_filename, testset_filename):
-        # read trainset_file
-        # extractd features, labels
-        # build model
-        # check accuracy on trainset
-        # check accuracy on testset
-
         df_train = pandas_helper.readData(trainset_filename, lendingclub_columns.get_dtypes_by_name())
-
-        self.buildFeaturesAndLabels(df_train)
-
-        feature_columns = lendingclub_helper.getFeatureColumns(df_train)
-        label_columns = lendingclub_helper.getLabelColumns(df_train)
-
-        print("features: {}".format(feature_columns))
-        print("labels: {}".format(label_columns))
-
-
-        X = df_train[feature_columns].values
-        y = df_train[label_columns[0]].values
-        # print(df_train[['f_annual_inc', 'f_sub_grade_A1', 'f_sub_grade_A2', 'f_sub_grade_A3', 'f_sub_grade_A4', 'f_sub_grade_A5', 'f_sub_grade_B1', 'f_sub_grade_B2', 'f_sub_grade_B3', 'f_sub_grade_B4', 'f_sub_grade_B5', 'f_sub_grade_C1', 'f_sub_grade_C2', 'f_sub_grade_C3', 'f_sub_grade_C4', 'f_sub_grade_C5', 'f_sub_grade_D1', 'f_sub_grade_D2', 'f_sub_grade_D3', 'f_sub_grade_D4', 'f_sub_grade_D5', 'f_sub_grade_E1', 'f_sub_grade_E2', 'f_sub_grade_E3', 'f_sub_grade_E4', 'f_sub_grade_E5', 'f_sub_grade_F1', 'f_sub_grade_F2', 'f_sub_grade_F4', 'f_sub_grade_F5', 'f_sub_grade_G1', 'f_sub_grade_G2', 'f_sub_grade_F3', 'f_sub_grade_G4', 'f_sub_grade_G5', 'f_sub_grade_NULL']])
-        # print(df_train[feature_columns])
-        # print(df_train[[label_columns]])
-        # print(df_train[feature_columns + label_columns])
-
-        model = scikit_helper.trainModel(X, y)
+        model = self.buildModel(df_train)
         print(model)
 
+        accuracy_train = self.getAccuracy(model, df_train)
+        print("Train accuracy: {}".format(accuracy_train))
 
+        df_test = pandas_helper.readData(testset_filename, lendingclub_columns.get_dtypes_by_name())
+        accuracy_test = self.getAccuracy(model, df_test)
+        print("Test accuracy: {}".format(accuracy_test))
+
+        # # Smoking section
+        # df_smoke = pd.DataFrame([
+        #     { "annual_inc": 100000, "sub_grade": "A1"},
+        #     { "annual_inc": 100000, "sub_grade": "B1"},
+        #     { "annual_inc": 100000, "sub_grade": "C1"},
+        #     { "annual_inc": 100000, "sub_grade": "D1"},
+        #     { "annual_inc": 100000, "sub_grade": "E1"},
+        #     { "annual_inc": 100000, "sub_grade": "F1"},
+        #     { "annual_inc": 100000, "sub_grade": "G1"},
+        # ])
+        # feature_column_defs = lendingclub_columns.get_columns_by_names(self.featurize_columns)
+        # lendingclub_helper.build_features_by_columns(df_smoke, feature_column_defs)
+
+        # for i in range(7):
+        #     print(df_smoke[[i]])
+        #     break
+        # accuracy_smoke = self.getAccuracy(model, df_smoke)
+        # print("Smoke accuracy: {}".format(accuracy_smoke))
+
+
+    def getAccuracy(self, model, df):
+        self.buildFeaturesAndLabels(df)
+        feature_columns = lendingclub_helper.getFeatureColumns(df)
+        label_columns = lendingclub_helper.getLabelColumns(df)
+
+        X = df[feature_columns].values
+        y = df[label_columns[0]].values
+
+        accuracy = scikit_helper.getAccuracy(model, X, y)
+        return accuracy
+
+    def buildModel(self, df):
+        self.buildFeaturesAndLabels(df)
+        feature_columns = lendingclub_helper.getFeatureColumns(df)
+        label_columns = lendingclub_helper.getLabelColumns(df)
+
+        X = df[feature_columns].values
+        y = df[label_columns[0]].values
+
+        model = scikit_helper.trainModel(X, y)
+        return model
 
     def buildFeaturesAndLabels(self, df):
-        feature_columns = lendingclub_columns.get_columns_by_names(["annual_inc", "sub_grade"])
-        # feature_columns = lendingclub_columns.get_columns_by_names(["annual_inc"])
-        lendingclub_helper.build_features_by_columns(df, feature_columns)
-        lendingclub_helper.buildCategorialLabel(df, "loan_status", lendingclub_helper.parse_loan_status)
+        feature_column_defs = lendingclub_columns.get_columns_by_names(self.featurize_columns)
+        lendingclub_helper.build_features_by_columns(df, feature_column_defs)
+        lendingclub_helper.buildCategorialLabel(df, self.labelize_columns[0], lendingclub_helper.parse_loan_status)
 
 
